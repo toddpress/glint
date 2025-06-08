bb39d7b92a7141b70fcf64c9af89df36e4f8a1aa
Todd Pressley <todd.pressley1@gmail.com>
2025-06-08 03:14:48 -0400

    Create stores.js

---
+++ src/core/stores.js
@@ -0,0 +1,18 @@
+// glint/stores/index.js
+const storeCache = new WeakMap();
+
+/**
+ * defineStore(factory: () => object): () => object
+ *
+ * Usage:
+ *   const useCounter = defineStore(() => {...});
+ *   const counter = useCounter(); // singleton instance per context
+ */
+export function defineStore(factory) {
+  return function useStore(context = globalThis) {
+    if (!storeCache.has(context)) {
+      storeCache.set(context, factory());
+    }
+    return storeCache.get(context);
+  };
+}
